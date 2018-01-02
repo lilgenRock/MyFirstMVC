b0b2ce1732acb6fa91ce5885f21fb1c9233baca9
lilgenRock <lilgen@hotmail.com>
2018-01-02 13:23:09 +0100

    Add project files.

---
+++ WebAppFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirstMVC.Models;
+
+namespace WebAppFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: About
+        public ActionResult About()
+        {
+            Person me = new Person(43, "Andreas Liljenberg");
+            return View(me);
+        }
+
+
+        // GET: People
+        public ActionResult People()
+        {
+            Person.People = new List<Person>();
+            Person.People.Add(new Person(40, "Fredrik"));
+            Person.People.Add(new Person(32, "Ali"));
+            Person.People.Add(new Person(26, "Irma"));
+            return View();
+        }
+
+    }
+}
---
+++ WebAppFirstMVC/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppFirstMVC.Models
+{
+    public class Person
+    {
+        public static List<Person> People { get; set; }
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+    }
+}
